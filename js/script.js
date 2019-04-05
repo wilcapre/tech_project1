53c03a3cc12c33b559023273a4c8e41eb762a2e5
willie capre <wcap18@gmail.com>
2019-04-05 10:57:09 -0400

    added files
    files

--- js/script.js
+++ js/script.js
@@ -6,15 +6,6 @@ project 1 - A Random Quote Generator
 // Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
 
 
-/*** 
- quote_one(); Create the array of quote objects and name it `quotes`.
-  Add at least five quote objects to the `quotes` array.
-  Give each quote object a `quote` and `source` property.
-  Add the `citation` property to at least one object in the array.
-  Add the `year` property to at least one object in the array.
-  Use console.log() to log your array of quotes to the console.
-***/
-
 /*
  This program will reqquest students to get quotes to print out output.  
  Quotes are taking from different sites, also needs to provide with names,
@@ -64,13 +55,6 @@ year: '1948'
 ];
 
 
-/***
-  Create the `getRandomQuote` function to:
-   - Create a variable to store a random number 
-   - Cse the random number to `return` a random quote object from the `quotes` array.
-***/
-
-
 /*
   Function getRandomQuote from quote_one to quote_five
   if quote matches the author's name, the quotation will be displayed;
@@ -88,16 +72,9 @@ function getRandomQuote () {
 console.log(getRandomQuote());
 
 /***
-  Create the `printQuote` function to: 
-   - Call the `getRandomQuote` function and assign it to a variable.
-   - Create a variable for the HTML string and set it equal to an empty string.
-   - Use the HTML template in the instructions or the markup in the index.html file, AND 
-     the random quote vairable to build your HTML string.
-   - Add the quote and source section to the HTML string.
-   - Use an if statement to check for the citation property before adding it to the HTML string.
-   - Use an if statement to check for the year property before adding it to the HTML string.
-   - Don't forget to close that final `p` tag.
-   - Set the `innerHTML` of the `quote-box` div to the HTML string. 
+  Using getRamdomQuote to call  printQuote function to return it string values. 
+  when call the quote and source function it will return it's  value if it is true,
+  but if it's not true the value will not return
 ***/
 
 
@@ -123,7 +100,7 @@ elements += "</p>";
 
 var quoteOuput = document.getElementById("quote-box"); 
 quoteOuput.innerHTML = elements;
-console.log ();
+
 }
 
 
